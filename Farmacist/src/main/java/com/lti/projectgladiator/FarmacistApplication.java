4116085319e203df02ecf4eec923da8b3c924c0b
DasTopian <shubham.dasgupta@lntinfotech.com>
2019-07-12 15:21:41 -0700

    Farmacist Entity Models

---
+++ Farmacist/src/main/java/com/lti/projectgladiator/FarmacistApplication.java
@@ -0,0 +1,13 @@
+package com.lti.projectgladiator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmacistApplication.class, args);
+	}
+
+}
